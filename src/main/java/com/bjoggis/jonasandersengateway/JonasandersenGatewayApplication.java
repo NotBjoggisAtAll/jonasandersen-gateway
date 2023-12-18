bbca892020c64792f916660cb183f41d08df9ec2
Jonas Bjørge Andersen <dev@jonasandersen.no>
2023-12-18 21:08:12 +0100

    Initial commit

---
+++ src/main/java/com/bjoggis/jonasandersengateway/JonasandersenGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bjoggis.jonasandersengateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonasandersenGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JonasandersenGatewayApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bjoggis/jonasandersengateway/JonasandersenGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjoggis.jonasandersengateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonasandersenGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
