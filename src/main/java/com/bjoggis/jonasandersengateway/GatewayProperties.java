673b018b950dfcfe043264f16f3da6f112090a4d
Jonas Bjørge Andersen <dev@jonasandersen.no>
2023-12-29 08:57:13 +0100

    Added option to also filter on path prefix

--- src/main/java/com/bjoggis/jonasandersengateway/GatewayProperties.java
+++ src/main/java/com/bjoggis/jonasandersengateway/GatewayProperties.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "jonas.gateway")
 public record GatewayProperties(Map<String, RouteInfo> routes) {
 
-  public record RouteInfo(List<String> patterns, String hostHeader) {
+  public record RouteInfo(List<String> patterns, String hostHeader, String pathPrefix) {
 
   }
 }
--- src/main/java/com/bjoggis/jonasandersengateway/JonasandersenGatewayApplication.java
+++ src/main/java/com/bjoggis/jonasandersengateway/JonasandersenGatewayApplication.java
@@ -1,13 +1,16 @@
 package com.bjoggis.jonasandersengateway;
 
 import com.bjoggis.jonasandersengateway.SubdomainRoutePredicateFactory.Config;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.gateway.handler.predicate.PathRoutePredicateFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder.Builder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.util.StringUtils;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -26,15 +29,27 @@ public class JonasandersenGatewayApplication {
   @Bean
   public RouteLocator customRouteLocator(RouteLocatorBuilder builder,
       SubdomainRoutePredicateFactory subdomainRoutePredicateFactory,
+      PathRoutePredicateFactory pathRoutePredicateFactory,
       GatewayProperties properties) {
 
     final Builder routes = builder.routes();
     for (GatewayProperties.RouteInfo routeInfo : properties.routes().values()) {
-      routes.route(r -> r.predicate(subdomainRoutePredicateFactory.apply(new Config(
-              routeInfo.patterns())))
-          .filters(f -> f.setHostHeader(routeInfo.hostHeader()))
-          .uri("http://kourier-internal.knative-serving.svc.cluster.local:80")
-      );
+      String pathPrefix = routeInfo.pathPrefix();
+      if (StringUtils.hasText(pathPrefix)) {
+        routes.route(r -> r
+            .predicate(subdomainRoutePredicateFactory.apply(new Config(routeInfo.patterns()))
+                .and(pathRoutePredicateFactory.apply(c -> c.setPatterns(List.of(pathPrefix)))))
+            .filters(f -> f.setHostHeader(routeInfo.hostHeader())
+                .rewritePath("%s/(?<segment>.*)".formatted(pathPrefix), "/${segment}"))
+            .uri("http://kourier-internal.knative-serving.svc.cluster.local:80")
+        );
+      } else {
+        routes.route(r -> r.predicate(subdomainRoutePredicateFactory.apply(new Config(
+                routeInfo.patterns())))
+            .filters(f -> f.setHostHeader(routeInfo.hostHeader()))
+            .uri("http://kourier-internal.knative-serving.svc.cluster.local:80")
+        );
+      }
     }
     return routes.build();
   }
